f57904fbbf59e0128038015d59c7c12167c62b1d
Domenico <d.rizzo@idsign.it>
2022-05-11 17:22:03 +0200

    Controller done

--- app/Http/Controllers/QuoteController.php
+++ app/Http/Controllers/QuoteController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Http;
 
 class QuoteController extends Controller
 {
     //
+    public function get() : JsonResponse
+    {
+        $response = Http::get('https://quotes.rest/qod');
+
+        if(!$response->ok()){
+            return response()->json([
+                'success' => false,
+                'message' => 'No quote sorry..'
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => $response->json('contents.quotes.quote'),
+            'credit' => 'They Say So (https://quotes.rest/)',
+        ]);
+    }
 }
